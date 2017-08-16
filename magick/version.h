966e3bcea63fec9baef05d8bb9d49a578e957097
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2017-08-16 08:28:19 -0500

    SVG: Fix buffer-overflow and inconsistent behavior in GetStyleTokens().

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x191600
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 19,16,0
-#define MagickChangeDate   "20170814"
-#define MagickReleaseDate  "snapshot-20170814"
+#define MagickChangeDate   "20170816"
+#define MagickReleaseDate  "snapshot-20170816"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
