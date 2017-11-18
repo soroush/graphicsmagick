e99665354e38f5c7641e46fca63312eca0856918
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2017-11-18 15:48:12 -0600

    tiff: Import libtiff 4.0.9.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x191600
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 19,16,0
-#define MagickChangeDate   "20171112"
-#define MagickReleaseDate  "snapshot-20171112"
+#define MagickChangeDate   "20171118"
+#define MagickReleaseDate  "snapshot-20171118"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
