61410efd3be707a223772ed461aa8e4f28efda1c
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2017-12-24 13:56:28 -0600

    BLOB: Fix writing to .gz file stream with Zlib 1.2.8.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x201701
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 20,17,1
-#define MagickChangeDate   "20171222"
-#define MagickReleaseDate  "snapshot-20171222"
+#define MagickChangeDate   "20171224"
+#define MagickReleaseDate  "snapshot-20171224"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
