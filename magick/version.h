de5db37e5700dbdb2f40a14115150218d088fb2f
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-03-24 13:01:18 -0500

    ReadFITSImage(): Include number of FITS scenes in file size validations.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x201702
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 20,17,2
-#define MagickChangeDate   "20180323"
-#define MagickReleaseDate  "snapshot-20180323"
+#define MagickChangeDate   "20180324"
+#define MagickReleaseDate  "snapshot-20180324"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
