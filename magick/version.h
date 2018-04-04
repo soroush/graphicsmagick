63a7290a0fb5259d24d49a549ced97538ff72a62
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-04-03 20:24:53 -0500

    Update generated files

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x201702
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 20,17,2
-#define MagickChangeDate   "20180402"
-#define MagickReleaseDate  "snapshot-20180402"
+#define MagickChangeDate   "20180403"
+#define MagickReleaseDate  "snapshot-20180403"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
