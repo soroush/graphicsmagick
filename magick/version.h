16a546205dd679b069d56e2d42dc02c5961c7e07
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-04-07 18:38:08 -0500

    PCD: Fixes to avoid double-free and memory leaks. (Credit to OSS-Fuzz)

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x201702
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 20,17,2
-#define MagickChangeDate   "20180406"
-#define MagickReleaseDate  "snapshot-20180406"
+#define MagickChangeDate   "20180407"
+#define MagickReleaseDate  "snapshot-20180407"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
