f65b8774cca2578f298f6b49e529ebceecb6bf24
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-04-06 20:15:31 -0500

    ReadPIXImage(): Detect EOF.  Reject RLE lenth of zero. (Credit to OSS-Fuzz)

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x201702
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 20,17,2
-#define MagickChangeDate   "20180405"
-#define MagickReleaseDate  "snapshot-20180405"
+#define MagickChangeDate   "20180406"
+#define MagickReleaseDate  "snapshot-20180406"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
