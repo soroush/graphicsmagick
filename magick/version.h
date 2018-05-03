5de332529797e6d0eb685f42d01baa3b6fbd66b1
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-05-03 08:46:19 -0500

    ReadMETAImage(): Detect and report 8BIMTEXT and 8BIMWTEXT decoding problems. (Credit to OSS-Fuzz)

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x211800
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 21,18,0
-#define MagickChangeDate   "20180502"
-#define MagickReleaseDate  "snapshot-20180502"
+#define MagickChangeDate   "20180503"
+#define MagickReleaseDate  "snapshot-20180503"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
