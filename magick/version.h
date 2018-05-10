7f23a2c1480d5076f710ce633367a91a623d21ed
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-05-10 08:29:08 -0500

    MIFF zlib/bzip compression tests are not expected to fail when zlib/bzlib are not available.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x211800
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 21,18,0
-#define MagickChangeDate   "20180505"
-#define MagickReleaseDate  "snapshot-20180505"
+#define MagickChangeDate   "20180510"
+#define MagickReleaseDate  "snapshot-20180510"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
