86a02e25ab564662a04b702740f598bf373ff4aa
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-05-21 08:34:49 -0500

    ReadTILEImage(): Remove size request while reading image to tile.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x211800
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 21,18,0
-#define MagickChangeDate   "20180520"
-#define MagickReleaseDate  "snapshot-20180520"
+#define MagickChangeDate   "20180521"
+#define MagickReleaseDate  "snapshot-20180521"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
