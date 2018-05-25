4a33fef987cdb42333a4a87d6300ce4ba7d4bc17
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-05-25 08:30:01 -0500

    fuzzing/oss-fuzz-build.sh: More fixes based on what is observed in oss-fuzz build log.

--- fuzzing/oss-fuzz-build.sh
+++ fuzzing/oss-fuzz-build.sh
@@ -4,6 +4,7 @@
 # eventually refactor it to be easy to run locally.
 
 # build zlib
+echo "=== Building zlib..."
 pushd "$SRC/zlib"
 ./configure --prefix="$WORK"
 make -j$(nproc) CFLAGS="$CFLAGS -fPIC"
@@ -11,13 +12,15 @@ make install
 popd
 
 # build xz
+echo "=== Building xz..."
 pushd "$SRC/xz"
 ./autogen.sh
-./configure --disable-xz --disable-xzdc --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --with-pic=yes --prefix="$WORK"
+PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure --disable-xz --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --with-pic=yes --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
 
+echo "=== Building libpng..."
 pushd "$SRC/libpng"
 autoreconf -fiv
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure --prefix="$WORK" CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib"
@@ -26,6 +29,7 @@ make install
 popd
 
 # build libjpeg
+echo "=== Building libjpeg..."
 pushd "$SRC/libjpeg-turbo"
 CFLAGS="$CFLAGS -fPIC" cmake . -DCMAKE_INSTALL_PREFIX="$WORK" -DENABLE_STATIC=on -DENABLE_SHARED=on
 make -j$(nproc)
@@ -33,22 +37,25 @@ make install
 popd
 
 # Build libtiff
+echo "=== Building libtiff..."
 pushd "$SRC/libtiff"
 autoreconf -fiv
-./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK"
+PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
 
 # Build liblcms2
+echo "=== Building lcms..."
 pushd "$SRC/Little-CMS"
 autoreconf -fiv
-./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK"
+PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK"
 make -j$(nproc)
 make install
 popd
 
 # Build freetype2
+echo "=== Building freetype2..."
 pushd "$SRC/freetype2"
 ./autogen.sh
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK" --enable-freetype-config
@@ -57,19 +64,26 @@ make install
 popd
 
 # Build webp
+echo "=== Building webp..."
 pushd "$SRC/libwebp"
 ./autogen.sh
-./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --enable-libwebpmux --prefix="$WORK" CFLAGS="$CFLAGS -fPIC"
+PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --enable-libwebpmux --prefix="$WORK" CFLAGS="$CFLAGS -fPIC"
 make -j$(nproc)
 make install
 popd
 
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include/libpng16 -I$WORK/include/freetype2 -I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK" --with-quantum-depth=16
+# freetype-config is in $WORK/bin so we temporarily add $WORK/bin to the path
+echo "=== Building GraphicsMagick..."
+PATH=$WORK/bin:$PATH PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include/libpng16 -I$WORK/include/freetype2 -I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK" --without-perl --with-quantum-depth=16
 make "-j$(nproc)"
 make install
 
-MAGICK_LIBS="$WORK/lib/libz.a $WORK/lib/liblzma.a $WORK/lib/libpng.a $WORK/lib/libtiff.a $WORK/lib/liblcms2.a $WORK/lib/libwebpmux.a $WORK/lib/libwebp.a $WORK/lib/libturbojpeg.a $WORK/lib/libfreetype.a"
+# Order libraries in linkage dependency order so libraries on the
+# right provide symbols needed by libraries to the left, to the
+# maximum extent possible.
+MAGICK_LIBS="$WORK/lib/libpng.a $WORK/lib/libtiff.a $WORK/lib/liblcms2.a $WORK/lib/libwebpmux.a $WORK/lib/libwebp.a $WORK/lib/libturbojpeg.a $WORK/lib/libfreetype.a $WORK/lib/liblzma.a $WORK/lib/libz.a "
 
+echo "=== Building fuzzers..."
 for f in fuzzing/*_fuzzer.cc; do
     fuzzer=$(basename "$f" _fuzzer.cc)
     # coder_fuzzer is handled specially below.
