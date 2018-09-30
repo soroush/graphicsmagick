302a059ca347fd491fa1b763ba29e0ee03241dd4
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-09-30 12:55:45 -0500

    DestroyCacheInfo(): Eliminate switch statements entirely and split unrelated logic.

--- magick/pixel_cache.c
+++ magick/pixel_cache.c
@@ -2032,56 +2032,40 @@ DestroyCacheInfo(Cache cache_info)
       return;
     }
   UnlockSemaphoreInfo(cache_info->reference_semaphore);
-  switch (cache_info->type)
+
+  /*
+    Release Cache Pixel Resources
+  */
+  if (MemoryCache == cache_info->type)
     {
-    default:
-      {
-        if (cache_info->pixels != (PixelPacket *) NULL)
-          {
-            MagickFreeMemory(cache_info->pixels);
-            LiberateMagickResource(MemoryResource,cache_info->length);
-          }
-        break;
-      }
-    case MemoryCache:
-      {
-        MagickFreeMemory(cache_info->pixels);
-        LiberateMagickResource(MemoryResource,cache_info->length);
-        break;
-      }
-    case MapCache:
-      {
-        (void) UnmapBlob(cache_info->pixels,(size_t) cache_info->length);
-        LiberateMagickResource(MapResource,cache_info->length);
-        if (cache_info->file != -1)
-          {
-            (void) close(cache_info->file);
-            LiberateMagickResource(FileResource,1);
-          }
-        cache_info->file=(-1);
-        (void) LiberateTemporaryFile(cache_info->cache_filename);
-        (void) LogMagickEvent(CacheEvent,GetMagickModule(),
-                              "remove %.1024s (%.1024s)",cache_info->filename,
-                              cache_info->cache_filename);
-        LiberateMagickResource(DiskResource,cache_info->length);
-        break;
-      }
-    case DiskCache:
-      {
-        if (cache_info->file != -1)
-          {
-            (void) close(cache_info->file);
-            LiberateMagickResource(FileResource,1);
-          }
-        cache_info->file=(-1);
-        (void) LiberateTemporaryFile(cache_info->cache_filename);
-        (void) LogMagickEvent(CacheEvent,GetMagickModule(),
-                              "remove %.1024s (%.1024s)",cache_info->filename,
-                              cache_info->cache_filename);
-        LiberateMagickResource(DiskResource,cache_info->length);
-        break;
-      }
+      MagickFreeMemory(cache_info->pixels);
+      LiberateMagickResource(MemoryResource,cache_info->length);
     }
+  else if (MapCache == cache_info->type)
+    {
+      (void) UnmapBlob(cache_info->pixels,(size_t) cache_info->length);
+      cache_info->pixels = NULL;
+      LiberateMagickResource(MapResource,cache_info->length);
+    }
+
+  /*
+    Release Cache File Resources
+  */
+  if ((MapCache == cache_info->type) || (DiskCache == cache_info->type))
+    {
+      if (cache_info->file != -1)
+        {
+          (void) close(cache_info->file);
+          LiberateMagickResource(FileResource,1);
+          cache_info->file=(-1);
+        }
+      (void) LiberateTemporaryFile(cache_info->cache_filename);
+      (void) LogMagickEvent(CacheEvent,GetMagickModule(),
+                            "remove %.1024s (%.1024s)",cache_info->filename,
+                            cache_info->cache_filename);
+      LiberateMagickResource(DiskResource,cache_info->length);
+    }
+
   DestroySemaphoreInfo(&cache_info->file_semaphore);
   DestroySemaphoreInfo(&cache_info->reference_semaphore);
   (void) LogMagickEvent(CacheEvent,GetMagickModule(),"destroy cache %.1024s",
