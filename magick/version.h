a446719b21207414a9c3c0911cffc61c3c0fcd52
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2018-11-21 08:04:39 -0600

    wand/wand_api.h: magick/api.h should be included prior to wand/wand_symbols.h

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x221900
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 22,19,0
-#define MagickChangeDate   "20181120"
-#define MagickReleaseDate  "snapshot-20181120"
+#define MagickChangeDate   "20181121"
+#define MagickReleaseDate  "snapshot-20181121"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
