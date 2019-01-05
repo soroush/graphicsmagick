a09a7da87bf9fe24c47e7fc3b1f997a2d57f88fe
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2019-01-05 09:53:23 -0600

    Request TIFF output from dcraw if build supports TIFF format.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x221900
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 22,19,0
-#define MagickChangeDate   "20181220"
-#define MagickReleaseDate  "snapshot-20181220"
+#define MagickChangeDate   "20190105"
+#define MagickReleaseDate  "snapshot-20190105"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
