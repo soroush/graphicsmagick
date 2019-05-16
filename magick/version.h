08d2f37c979577eea2490177dc99b8372c0d1e9b
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2019-05-16 18:53:14 -0500

    enum_strings.c: Make more data const.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x221900
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 22,19,0
-#define MagickChangeDate   "20190515"
-#define MagickReleaseDate  "snapshot-20190515"
+#define MagickChangeDate   "20190516"
+#define MagickReleaseDate  "snapshot-20190516"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
