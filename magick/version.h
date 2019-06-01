8efde5d20340dda4239ba20f6058bce98d1dcd6a
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2019-06-01 16:41:27 -0500

    LOGO: Tidy logo image definitions, and logo image output.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x221900
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 22,19,0
-#define MagickChangeDate   "20190523"
-#define MagickReleaseDate  "snapshot-20190523"
+#define MagickChangeDate   "20190601"
+#define MagickReleaseDate  "snapshot-20190601"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
