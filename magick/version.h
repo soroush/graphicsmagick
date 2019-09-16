db810a5750e831edfb0462464868363b39dfd55d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2019-09-16 08:33:01 -0500

    configure.ac: Report status of zstd (FaceBook Zstandard) compression in configuration summary.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x232001
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 23,20,1
-#define MagickChangeDate   "20190915"
-#define MagickReleaseDate  "snapshot-20190915"
+#define MagickChangeDate   "20190916"
+#define MagickReleaseDate  "snapshot-20190916"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
