5eec61e572204b146eadf282bc32a3ab6fa4b139
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2019-10-08 08:09:48 -0500

    ReadDPSImage(): Fix memory leak when OpenBlob() reports failure.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x232001
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 23,20,1
-#define MagickChangeDate   "20190927"
-#define MagickReleaseDate  "snapshot-20190927"
+#define MagickChangeDate   "20191008"
+#define MagickReleaseDate  "snapshot-20191008"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
