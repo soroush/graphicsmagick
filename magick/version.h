5f6a4b1c2126e34e3a4ea39f4dcfbf9eef72e788
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2019-12-07 08:39:59 -0600

    Update ChangeLog regarding Windows libtiff being updated to 4.1.0 release.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x232001
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 23,20,1
-#define MagickChangeDate   "20191124"
-#define MagickReleaseDate  "snapshot-20191124"
+#define MagickChangeDate   "20191207"
+#define MagickReleaseDate  "snapshot-20191207"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
