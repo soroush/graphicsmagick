7e6a842662929065eb73900af90e100cc7e83120
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2020-03-28 09:25:03 -0500

    fuzzing/oss-fuzz-build.sh: Remove mention of liblzma.a in the oss-fuzz build until its build problems get worked out.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x242100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 24,21,0
-#define MagickChangeDate   "20200327"
-#define MagickReleaseDate  "snapshot-20200327"
+#define MagickChangeDate   "20200328"
+#define MagickReleaseDate  "snapshot-20200328"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
