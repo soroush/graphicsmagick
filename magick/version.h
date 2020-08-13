1406a17f6400cbb8d05d01bc92250ca285c19f79
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2020-08-13 17:24:18 -0500

    configure.ac: Add --without-gdi32 configure option to support disabling use of the Microsoft Windows gdi32 library

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x242100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 24,21,0
-#define MagickChangeDate   "20200812"
-#define MagickReleaseDate  "snapshot-20200812"
+#define MagickChangeDate   "20200813"
+#define MagickReleaseDate  "snapshot-20200813"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
