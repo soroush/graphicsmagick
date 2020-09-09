fd4aba6e0eb9906363249bb77c427af456440276
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2020-09-09 16:19:45 -0500

    GetImageBoundingBox(): Fix SourceForge issue 345 "MagickTrimImage with extreme fuzz can produce image with negative width"

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x242100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 24,21,0
-#define MagickChangeDate   "20200816"
-#define MagickReleaseDate  "snapshot-20200816"
+#define MagickChangeDate   "20200909"
+#define MagickReleaseDate  "snapshot-20200909"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
