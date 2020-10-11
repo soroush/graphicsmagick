cbd365836091bb68dac540a9dac9ded97a6c929e
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2020-10-11 14:44:42 -0500

    Remove unnecessary comment

--- coders/pnm.c
+++ coders/pnm.c
@@ -1177,7 +1177,7 @@ static Image *ReadPNMImage(const ImageInfo *image_info,ExceptionInfo *exception)
           /*
             Allocate next image structure.
           */
-          AllocateNextImage(image_info,image); /* oss-fuzz 25247 memory leak */
+          AllocateNextImage(image_info,image);
           if (image->next == (Image *) NULL)
             {
               DestroyImageList(image);
