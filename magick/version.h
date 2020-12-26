fb84b6c10bbc4d3c46380fc61a3732d0c661f6b9
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2020-12-26 09:31:31 -0600

    bootstrap: Fix config.guess and config.sub download URLs.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x242100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 24,21,0
-#define MagickChangeDate   "20201225"
-#define MagickReleaseDate  "snapshot-20201225"
+#define MagickChangeDate   "20201226"
+#define MagickReleaseDate  "snapshot-20201226"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
