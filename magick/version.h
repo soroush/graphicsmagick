13dcf74ca7e3b033346a98c03440bf332ed5eebd
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2020-12-30 08:03:38 -0600

    Compilation warnings reduction.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x252200
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 25,22,0
-#define MagickChangeDate   "20201229"
-#define MagickReleaseDate  "snapshot-20201229"
+#define MagickChangeDate   "20201230"
+#define MagickReleaseDate  "snapshot-20201230"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
