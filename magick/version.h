9984ae84a68b317c98b5893c6d520036dc189263
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2021-09-18 09:52:31 -0500

    Fix ChangeLog entry and update generated files

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x252200
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 25,22,0
-#define MagickChangeDate   "20210917"
-#define MagickReleaseDate  "snapshot-20210917"
+#define MagickChangeDate   "20210918"
+#define MagickReleaseDate  "snapshot-20210918"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
