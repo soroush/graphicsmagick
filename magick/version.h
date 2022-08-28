f8c62b39a5f0c2567f2f725f919f2ea26473b7c9
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2022-08-28 09:16:21 -0500

    fuzzing/oss-fuzz-build.sh: Add messaging regarding building libxml2 and jasper

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x272400
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 27,24,0
-#define MagickChangeDate   "20220818"
-#define MagickReleaseDate  "snapshot-20220818"
+#define MagickChangeDate   "20220828"
+#define MagickReleaseDate  "snapshot-20220828"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
