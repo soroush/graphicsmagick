c3a65fec3bd23338aa6802f068c9d550dad489a7
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2022-10-05 17:18:16 -0500

    scripts/changelog2rst.sh: Try to deal with the crazy mix of spaces and hard tabs which are now appearing in the ChangeLog file.

--- scripts/changelog2rst.sh
+++ scripts/changelog2rst.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Simple filter to filter a ChangeLog to something in reStructuredText.
 #
-sed -e 's/	\* /  - /g ; s/	/    / ; s/\*/\\*/g; s/\_/\\_/g'
+expand | sed -e 's/^        \* /  - /g ; s/^        /    /g ; s/\*/\\*/g; s/\_/\\_/g'
