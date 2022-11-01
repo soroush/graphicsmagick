5a5867f3bc8ba422fcbd1c42a6d02a414cf47fcc
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2022-11-01 11:00:10 -0500

    MagickToMime(): Add MIME mappings for apng, avif, bmp, ico, and webp.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x272400
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 27,24,0
-#define MagickChangeDate   "20221031"
-#define MagickReleaseDate  "snapshot-20221031"
+#define MagickChangeDate   "20221101"
+#define MagickReleaseDate  "snapshot-20221101"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
