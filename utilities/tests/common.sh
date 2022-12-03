925bdf8b4536f6ea2db3bc4500ed1427a180a20e
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2022-12-03 15:23:30 -0600

    Changes intended to help tests under MinGW

--- utilities/tests/common.sh
+++ utilities/tests/common.sh
@@ -5,12 +5,12 @@ subdir=utilities/tests
 . ${top_srcdir}/scripts/tap-functions.shi
 mkdir -p ${subdir}
 cd ${subdir} || exit 1
-BETARGB_PROFILE="${top_srcdir}/utilities/tests/BetaRGB.icc"
-GENERIC_TTF="${top_srcdir}/PerlMagick/demo/Generic.ttf"
-MODEL_MIFF="${top_srcdir}/Magick++/demo/model.miff"
-SMILE_MIFF="${top_srcdir}/Magick++/demo/smile.miff"
-SUNRISE_JPEG="${top_srcdir}/utilities/tests/sunrise.jpg"
-SUNRISE_MIFF="${top_srcdir}/utilities/tests/sunrise.miff"
+BETARGB_PROFILE="${TOP_SRCDIR_NATIVE}/utilities/tests/BetaRGB.icc"
+GENERIC_TTF="${TOP_SRCDIR_NATIVE}/PerlMagick/demo/Generic.ttf"
+MODEL_MIFF="${TOP_SRCDIR_NATIVE}/Magick++/demo/model.miff"
+SMILE_MIFF="${TOP_SRCDIR_NATIVE}/Magick++/demo/smile.miff"
+SUNRISE_JPEG="${TOP_SRCDIR_NATIVE}/utilities/tests/sunrise.jpg"
+SUNRISE_MIFF="${TOP_SRCDIR_NATIVE}/utilities/tests/sunrise.miff"
 #CONVERT_FLAGS='-monitor'
 #COMPOSITE_FLAGS='-monitor'
 #MONTAGE_FLAGS='-monitor'
