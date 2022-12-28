9f36d628465fa48b04b8eb8479f5a706e8e1a12a
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2022-12-28 09:11:04 -0600

    utilities/tests/convert-cmds.txt: Add more convert command permutations

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x272401
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 27,24,1
-#define MagickChangeDate   "20221226"
-#define MagickReleaseDate  "snapshot-20221226"
+#define MagickChangeDate   "20221228"
+#define MagickReleaseDate  "snapshot-20221228"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
