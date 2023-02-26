3a2b81f0b045e9d0816add923087f81eff9c8295
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2023-02-26 12:13:22 -0600

    Makefile.am: Stop producing BZip compressed archives.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x272402
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 27,24,2
-#define MagickChangeDate   "20230212"
-#define MagickReleaseDate  "snapshot-20230212"
+#define MagickChangeDate   "20230226"
+#define MagickReleaseDate  "snapshot-20230226"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
