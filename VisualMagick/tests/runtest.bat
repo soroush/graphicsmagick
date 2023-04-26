162e78943cf7d3ff1090eaa13c747d6ae63aa562
Jaroslav Fojtik <none@none>
2023-04-26 03:48:41 +0200

    VisualMagick/tests/runtest.bat Added new tests for all possible fileformats. XTRNIMAGE causes segfault - this should be fixed.

--- VisualMagick/tests/runtest.bat
+++ VisualMagick/tests/runtest.bat
@@ -38,7 +38,11 @@ call :_dorwtest both ART
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both AVS
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both B
+if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both BIE
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both BIGTIFF
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both BMP
 if not %COUNT% EQU 6 goto :_failed
@@ -46,14 +50,24 @@ call :_dorwtest both BMP2
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both BMP3
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both BRF
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both C
+if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both CAL
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both CALS
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both CIN
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest file CLIPBOARD
+if not %COUNT% EQU 3 goto :_failed
 call :_dorwtest both CMYK
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both CMYKA
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both DCM
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both DCX
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both DIB
@@ -61,10 +75,6 @@ if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both DPX
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both EPDF
-if not %COUNT% EQU 6 goto :_failed
-call :_dorwtest both EPSF
-if not %COUNT% EQU 6 goto :_failed
-call :_dorwtest both EPSI
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both EPI
 if not %COUNT% EQU 6 goto :_failed
@@ -86,27 +96,102 @@ call :_dorwtest both EPT3
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both FAX
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both FILE
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both FITS
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both FPX
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both FTP
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both G
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both G3
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both GIF
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both GIF87
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both GRADIENT
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both GRAY
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both GRAYA
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both GROUP4RAW
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both H
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HEIC
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HEIF
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HRZ
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HTM
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HEIC
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HEIF
+@rem if not %COUNT% EQU 6 goto :_failed
 @rem This test is failing!
 call :_dorwtest both HTML
 @rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both HTTP
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both ICB
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ICC
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ICM
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ICO
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ICODIB
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ICON
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both IM1
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both IM24
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both IM8
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both IMAGE
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ISOBRL
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both ISOBRL6
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both J2C
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both JBG
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both JBIG
 if not %COUNT% EQU 6 goto :_failed
-call :_dorwtest both JNG
+call :_dorwtest both JNG                     	
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both JP2
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both JPEG
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both JPG
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both JXL
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both K
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both LABEL
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both LOCALE
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both LOGO
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both M
+if not %COUNT% EQU 6 goto :_failed
+@rem This test is failing!
+call :_dorwtest both M2V
+@rem if not %COUNT% EQU 6 goto :_failed
 @rem This test is failing!
 call :_dorwtest both MAP
 @rem if not %COUNT% EQU 6 goto :_failed
@@ -114,24 +199,64 @@ call :_dorwtest both MAT
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both MIFF
 if not %COUNT% EQU 6 goto :_failed
-call :_dorwtest both MNG
-if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both MONO
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest file MPC
 if not %COUNT% EQU 3 goto :_failed
+call :_dorwtest both MNG
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest file MPC
+if not %COUNT% EQU 3 goto :_failed
+call :_dorwtest both MPEG
+@rem not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both MPG
+@rem not %COUNT% EQU 6 goto :_failed
+call :_dorwtest file MPR
+if not %COUNT% EQU 3 goto :_failed
+@rem call :_dorwtest both MPRI
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both MSL
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both MTV
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both MVG
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both NETSCAPE
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both O
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both OTB
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both P7
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PAL
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PALM
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PAM
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PATTERN
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PBM
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PCD
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PCDS
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PCL
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PCT
+if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PCX
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PDB
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PDF
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PFA
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PFB
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PGM
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PGX
@@ -140,17 +265,35 @@ call :_dorwtest both PICON
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PICT
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PIX
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PM
+if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PNG
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PNG8
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PNG32
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PNM
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PPM
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PREVIEW
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PS
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PS2
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both PS3
 if not %COUNT% EQU 6 goto :_failed
 @rem call :_dorwtest both PSD
 @rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both PTIF
+if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both PWP
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both R
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both RAS
 if not %COUNT% EQU 6 goto :_failed
@@ -158,22 +301,48 @@ call :_dorwtest both RGB
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both RGBA
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both RLA
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both RLE
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both SCT
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both SGI
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both STEGANO
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both SUN
+if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both SVG
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both TEXT
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both TGA
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both TIF
+if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both TIFF
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both TIM
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both TTF
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both TXT
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both UBRL
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both UBRL6
+@rem if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both UIL
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both UYVY
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both VDA
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both VICAR
 if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both VID
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both VIFF
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both VST
@@ -184,12 +353,24 @@ call :_dorwtest both WEBP
 if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both WPG
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both X
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both XBM
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both XMP
+@rem if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both XPM
 if not %COUNT% EQU 6 goto :_failed
+@rem call :_dorwtest both XTRNARRAY
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest both XTRNIMAGE
+@rem if not %COUNT% EQU 6 goto :_failed
+call :_dorwtest none XV
+if not %COUNT% EQU 0 goto :_failed
 call :_dorwtest none XWD
 if not %COUNT% EQU 0 goto :_failed
+call :_dorwtest both Y
+if not %COUNT% EQU 6 goto :_failed
 call :_dorwtest both YUV
 if not %COUNT% EQU 6 goto :_failed
 echo %TESTS% tests completed!
