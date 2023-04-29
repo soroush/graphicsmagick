d6b6c3b2cff6117fb1adb90526633abfcfe4d3a2
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2023-04-29 11:43:11 -0500

    RoundDoubleToQuantumN(): Version of RoundDoubleToQuantum() which handles NaN.  Use where needed.

--- coders/viff.c
+++ coders/viff.c
@@ -1,5 +1,5 @@
 /*
-% Copyright (C) 2003-2020 GraphicsMagick Group
+% Copyright (C) 2003-2023 GraphicsMagick Group
 % Copyright (C) 2002 ImageMagick Studio
 % Copyright 1991-1999 E. I. du Pont de Nemours and Company
 %
@@ -78,7 +78,7 @@ static unsigned int
 typedef struct _ViffInfo
 {
   unsigned char
-  identifier,
+    identifier,
     file_type,
     release,
     version,
@@ -735,13 +735,8 @@ static Image *ReadVIFFImage(const ImageInfo *image_info,
       if (viff_info.map_scheme == VFF_MS_NONE)
         {
           value=(value-min_value)*scale_factor;
-          if (value > MaxRGB)
-            value=MaxRGB;
-          else
-            if (value < 0)
-              value=0;
         }
-      *p=(Quantum) value;
+      *p=RoundDoubleToQuantumN(value);
       p++;
     }
     /*
