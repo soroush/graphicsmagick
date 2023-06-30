fceb60a2895fb497e2144cf8bf7cb0add49b0f5d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2023-06-30 09:10:29 -0500

    fuzzing/oss-fuzz-build.sh: Produce more build information in fuzzing build

--- fuzzing/oss-fuzz-build.sh
+++ fuzzing/oss-fuzz-build.sh
@@ -4,7 +4,7 @@
 # eventually refactor it to be easy to run locally.
 
 # build zlib
-echo "=== Building zlib..."
+printf "=== Building zlib...\n"
 pushd "$SRC/zlib"
 ./configure --prefix="$WORK"
 make -j$(nproc) CFLAGS="$CFLAGS -fPIC"
@@ -12,7 +12,7 @@ make install
 popd
 
 # build xz
-#echo "=== Building xz..."
+printf "=== Building xz...\n"
 pushd "$SRC/xz"
 ./autogen.sh --no-po4a --no-doxygen
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure --disable-xz --disable-lzmadec --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --prefix="$WORK"
@@ -21,13 +21,13 @@ make install
 popd
 
 # build zstd
-echo "==== Building zstd..."
+printf"==== Building zstd...\n"
 pushd "$SRC/zstd"
 make -j$(nproc) lib-release
 make install PREFIX="$WORK"
 popd
 
-echo "=== Building libpng..."
+printf "=== Building libpng...\n"
 pushd "$SRC/libpng"
 autoreconf -fiv
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure --prefix="$WORK" CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib"
@@ -36,7 +36,7 @@ make install
 popd
 
 # build libjpeg
-echo "=== Building libjpeg..."
+printf "=== Building libjpeg...\n"
 pushd "$SRC/libjpeg-turbo"
 CFLAGS="$CFLAGS -fPIC" cmake . -DCMAKE_INSTALL_PREFIX="$WORK" -DENABLE_STATIC=on -DENABLE_SHARED=on -DWITH_JPEG8=1 -DWITH_SIMD=0
 make -j$(nproc)
@@ -44,7 +44,7 @@ make install
 popd
 
 # Build webp
-echo "=== Building webp..."
+printf "=== Building webp...\n"
 pushd "$SRC/libwebp"
 ./autogen.sh
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --enable-libwebpmux --prefix="$WORK" CFLAGS="$CFLAGS -fPIC"
@@ -53,7 +53,7 @@ make install
 popd
 
 # Build libtiff
-echo "=== Building libtiff..."
+printf "=== Building libtiff...\n"
 pushd "$SRC/libtiff"
 autoreconf -fiv
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK" --disable-old-jpeg
@@ -62,7 +62,7 @@ make install
 popd
 
 # Build liblcms2
-echo "=== Building lcms..."
+printf "=== Building lcms...\n"
 pushd "$SRC/Little-CMS"
 autoreconf -fiv
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK"
@@ -71,7 +71,7 @@ make install
 popd
 
 # Build freetype2
-echo "=== Building freetype2..."
+printf "=== Building freetype2...\n"
 pushd "$SRC/freetype2"
 ./autogen.sh
 PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK" --enable-freetype-config
@@ -79,14 +79,14 @@ make -j$(nproc)
 make install
 popd
 
-#echo "=== Building libxml2..."
+#printf "=== Building libxml2...\n"
 #pushd "$SRC/libxml2"
 #./autogen.sh --disable-shared --without-debug --without-legacy --without-python --without-schematron --without-schemas --prefix="${WORK}"
 #make -j$(nproc)
 #make install
 #popd
 
-echo "=== Building jasper..."
+printf "=== Building jasper...\n"
 pushd "$SRC/jasper"
 cmake -G "Unix Makefiles" -H. -Bstaging -DJAS_ENABLE_SHARED=false -DCMAKE_INSTALL_PREFIX=$WORK #-DJAS_INCLUDE_BMP_CODEC=false -DJAS_INCLUDE_JPG_CODEC=false -DJAS_INCLUDE_MIF_CODEC=false -DJAS_INCLUDE_PNM_CODEC=false -DJAS_INCLUDE_RAS_CODEC=false -DJAS_ENABLE_OPENGL=false -DJAS_ENABLE_LIBJPEG=false -DJAS_ENABLE_OPENGL=false -DJAS_ENABLE_LIBHEIF=false
 pushd staging
@@ -96,7 +96,7 @@ popd
 popd
 
 # freetype-config is in $WORK/bin so we temporarily add $WORK/bin to the path
-echo "=== Building GraphicsMagick..."
+printf "=== Building GraphicsMagick...\n"
 PATH=$WORK/bin:$PATH PKG_CONFIG_PATH="$WORK/lib/pkgconfig" ./configure CPPFLAGS="-I$WORK/include/libpng16 -I$WORK/include/freetype2 -I$WORK/include/libxml2 -I$WORK/include" CFLAGS="$CFLAGS" LDFLAGS="${LDFLAGS:-} -L$WORK/lib" --prefix="$WORK" --disable-compressed-files --without-perl --with-quantum-depth=16
 make "-j$(nproc)"
 make install
@@ -115,8 +115,9 @@ for lib in libxml2.a libjasper.a libpng.a libtiff.a liblcms2.a libwebpmux.a libw
         MAGICK_LIBS="${MAGICK_LIBS}${WORK}/lib/${lib}"
     fi
 done
+printf "MAGICK_LIBS=${MAGICK_LIBS}\n"
 
-echo "=== Building fuzzers..."
+printf "=== Building fuzzers...\n"
 for f in fuzzing/*_fuzzer.cc; do
     fuzzer=$(basename "$f" _fuzzer.cc)
     # coder_fuzzer is handled specially below.
@@ -124,15 +125,23 @@ for f in fuzzing/*_fuzzer.cc; do
         continue
     fi
 
+    target="$OUT/${fuzzer}_fuzzer"
+    printf "=== Building ${target}...\n"
+    set -x
     $CXX $CXXFLAGS -std=c++11 -I "$WORK/include/GraphicsMagick" \
-        "$f" -o "$OUT/${fuzzer}_fuzzer" \
+        "$f" -o "${target}" \
         $LIB_FUZZING_ENGINE "$WORK/lib/libGraphicsMagick++.a" \
         "$WORK/lib/libGraphicsMagick.a" $MAGICK_LIBS
+    set +x
 done
 
+target="$WORK/coder_list"
+printf "=== Building ${target}...\n"
+set -x
 $CXX $CXXFLAGS -std=c++11 -I "$WORK/include/GraphicsMagick" \
-    fuzzing/coder_list.cc -o "$WORK/coder_list" \
+    fuzzing/coder_list.cc -o "${target}" \
     "$WORK/lib/libGraphicsMagick++.a" "$WORK/lib/libGraphicsMagick.a" $MAGICK_LIBS
+set +x
 
 for item in $("$WORK/coder_list"); do
     coder="${item:1}"
@@ -141,10 +150,14 @@ for item in $("$WORK/coder_list"); do
         coder_flags="$coder_flags -DFUZZ_GRAPHICSMAGICK_CODER_WRITE=1"
     fi
 
+    target="${OUT}/coder_${coder}_fuzzer"
+    printf "=== Building ${target}...\n"
+    set -x
     $CXX $CXXFLAGS -std=c++11 -I "$WORK/include/GraphicsMagick" \
-        fuzzing/coder_fuzzer.cc -o "${OUT}/coder_${coder}_fuzzer" \
+        fuzzing/coder_fuzzer.cc -o "${target}" \
         $coder_flags $LIB_FUZZING_ENGINE "$WORK/lib/libGraphicsMagick++.a" \
         "$WORK/lib/libGraphicsMagick.a" $MAGICK_LIBS
+    set +x
 
     if [ -f "fuzzing/dictionaries/${coder}.dict" ]; then
         cp "fuzzing/dictionaries/${coder}.dict" "${OUT}/coder_${coder}_fuzzer.dict"
